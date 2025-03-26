c1c608b1a0c7e087ff1c8bc3a535c609d0c71fd0
Siyam35472 <dxxdxxx8@gmail.com>
2025-03-26 19:17:01 +0600

    Add files via upload

---
+++ script1 (1).js
@@ -0,0 +1,5 @@
+// Use the following to include this JavaScript file 
+// <script src="script1.js"></script>
+var i = 10;document.addEventListener("DOMContentLoaded", function() {
+    console.log("Casino site loaded!");
+});
